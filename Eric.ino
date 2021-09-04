820b8fc363b817afcd81d8e7ff4088e56e1bed51
ERIC5911 <82673699+ERIC5911@users.noreply.github.com>
2021-09-04 23:23:52 +0530

    Add files via upload

---
+++ Eric.ino
@@ -0,0 +1,59 @@
+/**
+ * Name- Aditya Sheoran
+ * Student ID- 2010993030
+ */
+void Point()
+{
+   digitalWrite(LED_BUILTIN, HIGH);     
+   delay(200);                          
+   digitalWrite(LED_BUILTIN, LOW);    
+   delay(200);                           
+void Line()
+{
+   digitalWrite(LED_BUILTIN, HIGH);       
+   delay(1000);                           
+   digitalWrite(LED_BUILTIN, LOW);      
+   delay(1000);                           
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  // A (.-)
+    Point();                         
+    Line();                        
+    delay(2000);
+
+    //D (-..)
+    Line();
+    Point();
+    Point();
+    delay(2000);
+
+    //I (..)
+    Point();
+    Point();
+    delay(2000);
+
+    //T (-)
+    Line();
+    delay(2000);
+
+    //Y (-.--)
+    Line();
+    Point();
+    Line();
+    Line();
+    delay(2000);
+  
+    //A (.-)
+    Point();
+    Line();
+    delay(2000);
+}
+
+
+  
